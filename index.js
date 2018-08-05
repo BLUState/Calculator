6e379339bc873f46e1344b75d73442d3e19fa86b
BLUState <40577804+BLUState@users.noreply.github.com>
2018-08-05 16:07:04 -0400

    Delete index.js
    Removed unnecessary JavaScript file

--- index.js
+++
@@ -1,6 +0,0 @@
-function deleteLast()
-{
-    var string = document.getElementById("display").value;
-    var newString = string.substring(0, string.length - 1);
-    document.getElementById("display").value = newString;
-}
